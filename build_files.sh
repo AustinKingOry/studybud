e14328b810d66603b3da1914ede8d672536273ca
The White Hat KE <austinkings30@gmail.com>
2024-04-12 20:00:23 +0300

    update builds

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,16 @@
-# build_files.sh
-pip install -r requirements.txt
+#!/bin/bash
+echo "Creating a virtual environment"
+python3.9 -m virtualenv .
 # virtualenv env
-# env/scripts/activate
-python3.9 manage.py collectstatic
+echo "Activating the env"
+source bin/activate
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
