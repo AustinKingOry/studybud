7ae3a65399cef1da11152ff5f00ec57a2c0d60ba
The White Hat KE <austinkings30@gmail.com>
2024-04-12 20:16:09 +0300

    upadate builds

--- build_files.sh
+++ build_files.sh
@@ -3,11 +3,7 @@ echo "Getting pip"
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
 
-echo "Creating a virtual environment"
-python3.9 -m pip install virtualenv
-virtualenv env
-echo "Activating the env"
-source env/bin/activate
+
 # Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
